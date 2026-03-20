f7a3104afb34844664230a585de72a936007bd43
henrik <henriktomt@gmail.com>
2026-03-20 07:56:01 +0100

    snippet fix

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -1,6 +1,16 @@
 return {
 	{
 		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"saadparwaiz1/cmp_luasnip",
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
+			"petertriho/cmp-git",
+		},
 		opts = function(_, opts)
 			opts.sorting = opts.sorting or {}
 			opts.sorting.comparators = opts.sorting.comparators or {}
@@ -35,11 +45,6 @@ return {
 					{ name = "path" },
 					--{ name = "nvim_lua" },
 					--{ name = "git" },
-					{
-						option = {
-							ignore_cmds = { "Man", "!" },
-						},
-					},
 				}, {
 					{ name = "buffer" },
 				}),
